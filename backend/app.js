4d00824313dee5eb4ee0439bca4f50618db10737
haritha380 <bandaraindika488@gmail.com>
2025-02-27 17:18:24 +0530

    gui project

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const userRouter = require("./routes/userRouter");
+const errorHandler = require("./middlewares/errorHandlerMiddleware");
+const categoryRouter = require("./routes/categoryRouter");
+const transactionRouter = require("./routes/transactionRouter");
+const app = express();
+//!Connect to mongodb
+mongoose
+ .connect("mongodb+srv://Haritha:Haritha1234@mongo4961.h9x21.mongodb.net/")
+ .then(() => console.log("DB Connected"))
+ .catch((e) => console.log(e));
+ //!Middlewares
+ app.use(express.json()); //?pass incoming jason data
+//!Routes
+app.use("/",userRouter);
+app.use("/",categoryRouter);
+app.use("/",transactionRouter);
+//!Error 
+app.use(errorHandler);
+
+//!Start the server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () =>
+  console.log(`Server is running on this port... ${PORT} `)
+);
+
+
+
